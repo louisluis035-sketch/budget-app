931ee00ecd584a13bfdeb8434692c3f8f3e75e08
louisluis035-sketch <louisluis035@gmail.com>
2026-07-17 18:03:12 +0300

    Change database user and password in config.php
    Updated database credentials for security.

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return [
     "db" => [
         "host" => "127.0.0.1",
         "name" => "budget_app",
-        "user" => "root",
-        "pass" => "",
+        "user" => "budget_user",
+        "pass" => "StrongPass123!", // weka password halisi uliyotengeneza
         "charset" => "utf8mb4",
     ],
     "encryption_key" => "ChangeThisToAStrongRandomKey12345",
